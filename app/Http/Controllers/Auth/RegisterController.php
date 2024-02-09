cebcddc48f519c5dc98dc340e65c23db564a53b7
ChiaHaoSun <hhhhh2953@gmail.com>
2024-02-09 11:36:27 +0800

    將首頁改為登入頁。

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -73,7 +73,7 @@ class RegisterController extends Controller
 
         return $request->wantsJson()
             ? new JsonResponse([], 201)
-            : redirect($this->redirectPath());
+            : redirect()->route('index');
     }
 
     /**
