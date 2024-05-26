6e7bafcbec0b8e1f2375c4db7e8e18c12dfa4bda
ChiaHaoSun <hhhhh2953@gmail.com>
2024-05-26 19:08:22 +0800

    安裝 laravel/telescope 套件

---
+++ app/Providers/TelescopeServiceProvider.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Laravel\Telescope\IncomingEntry;
+use Laravel\Telescope\Telescope;
+use Laravel\Telescope\TelescopeApplicationServiceProvider;
+
+class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        /**
+         * 在晚上 7 點至早上 7 點間，啟用暗色樣式。
+         */
+        if (now()->hour >= 19 || now()->hour < 7) {
+            Telescope::night(); //啟用暗色樣式
+        }
+
+        $this->hideSensitiveRequestDetails();
+
+        $isLocal = $this->app->environment('local');
+
+        Telescope::filter(function (IncomingEntry $entry) use ($isLocal) {
+            return $isLocal ||
+                    $entry->isReportableException() ||
+                    $entry->isFailedRequest() ||
+                    $entry->isFailedJob() ||
+                    $entry->isScheduledTask() ||
+                    $entry->hasMonitoredTag();
+        });
+    }
+
+    /**
+     * Prevent sensitive request details from being logged by Telescope.
+     */
+    protected function hideSensitiveRequestDetails(): void
+    {
+        if ($this->app->environment('local')) {
+            return;
+        }
+
+        Telescope::hideRequestParameters(['_token']);
+
+        Telescope::hideRequestHeaders([
+            'cookie',
+            'x-csrf-token',
+            'x-xsrf-token',
+        ]);
+    }
+
+    /**
+     * Register the Telescope gate.
+     *
+     * This gate determines who can access Telescope in non-local environments.
+     */
+    protected function gate(): void
+    {
+        //暫時無解方
+        Gate::define('viewTelescope', function ($user) {
+            return in_array($user->email, [
+                'admin@gmail.com',
+            ]);
+        });
+    }
+}
